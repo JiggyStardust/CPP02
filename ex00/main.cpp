bc4861cf56e625922df6096a768325325cee562d
JiggyStardust <saaraleni@gmail.com>
2025-04-17 13:55:00 +0300

    Added setRawBits to main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sniemela <sniemela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 08:54:57 by sniemela          #+#    #+#             */
-/*   Updated: 2025/03/26 09:32:14 by sniemela         ###   ########.fr       */
+/*   Updated: 2025/04/17 13:52:54 by sniemela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,11 @@ int main( void )
 	Fixed c;
 	c = b;
 	std::cout << a.getRawBits() << std::endl;
+	a.setRawBits(5);
+	std::cout << a.getRawBits() << std::endl;
 	std::cout << b.getRawBits() << std::endl;
 	std::cout << c.getRawBits() << std::endl;
+
+
 	return 0;
 }
