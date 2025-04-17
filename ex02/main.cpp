0ca20f5564faa8fa2a06086b2e65a8cee638f4b0
JiggyStardust <saaraleni@gmail.com>
2025-04-17 13:49:01 +0300

    More tests put into main

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: sniemela <sniemela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 18:08:51 by sniemela          #+#    #+#             */
-/*   Updated: 2025/03/27 14:47:20 by sniemela         ###   ########.fr       */
+/*   Updated: 2025/04/17 13:47:46 by sniemela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,11 @@ int main( void )
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
+	std::cout << --a << std::endl;
+	std::cout << a-- << std::endl;
 	std::cout << b << std::endl;
+	std::cout << b / 3 << std::endl;
+
 	std::cout << Fixed::max( a, b ) << std::endl;
 	std::cout << Fixed::min( a, b ) << std::endl;
 
@@ -45,19 +49,23 @@ int main( void )
 	std::cout << Fixed::min( c, d ) << std::endl;
 
 	std::cout << "\n\nTests part 3 test the rest of the operands." << std::endl;
-	std::cout << "a: " << a << "b: " << b << "c: " << c << "d: " << d << std::endl;
+	std::cout << "a: " << a << " b: " << b << " c: " << c << " d: " << d << std::endl;
 	if (a <= b)
-		std::cout << "a is smaller or equal to b." << std::endl;
-	else
-		std::cout << "b is smaller or equal to a." << std::endl;
+		std::cout << "1. a is smaller or equal to b." << std::endl;
+	if (b <= a)
+		std::cout << "1. b is smaller or equal to a." << std::endl;
+	if (a < b)
+		std::cout << "2. a is smaller than b." << std::endl;
+	if (a > b)
+		std::cout << "2. b is smaller than a." << std::endl;
 	if (a >= b)
-		std::cout << "a is bigger or equal to b." << std::endl;
-	else
-		std::cout << "b is bigger or equal to a." << std::endl;
+		std::cout << "3. a is bigger or equal to b." << std::endl;
+	if (b >= a)
+		std::cout << "3. b is bigger or equal to a." << std::endl;
 	if (b != d)
-		std::cout << "b is not equal to d." << std::endl;
+		std::cout << "4. b is not equal to d." << std::endl;
 	if (c == d)
-		std::cout << "c is equal to d." << std::endl;
+		std::cout << "5. c is equal to d." << std::endl;
 
 	return 0;
 }
